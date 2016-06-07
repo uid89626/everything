bfb2699c2c5f5e32d1d3124c6889832eb33a421a
uid89626 <uid89626@gmail.com>
2016-06-07 11:56:44 -0500

    setup.py
    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 12:53:50 2015
+
+@author: SESA72332
+"""
+
+from setuptools import setup
+
+setup(
+    name="Everything",
+    version="1.0",
+    description="Everything.",
+    author='Aaron S. Carlton',
+    author_email='uid89626@gmail.com',
+#    install_requires=[
+#        "requests",
+#        "bcrypt",
+#    ],
+    packages=['mt']
+)
